32931f741cff808d4ad382f02d8fc5ec60445ef2
Jeffrey Rennie <rennie@google.com>
2015-12-03 13:59:38 -0800

    HelloWorld sample.
    Change-Id: I874209538ccf4f29e86a6ecf8c8f204ea0046ede

---
+++ 1-hello-world/Startup.cs
@@ -0,0 +1,37 @@
+﻿// Copyright(c) 2015 Google Inc.
+
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+
+namespace GoogleCloudSamples
+{
+    public class Startup
+    {
+        // This method gets called by the runtime.
+        // Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args) =>
+            WebApplication.Run<Startup>(args);
+    }
+}
