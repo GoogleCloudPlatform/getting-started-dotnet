8f0afda9742cbadef7b67d23ec3f0acd1a994600
Jeffrey Rennie <rennie@google.com>
2016-02-22 13:34:52 -0800

    Clean up some stray license notices that were accidentally added by codeformatter.

--- aspnet/1-hello-world/App_Start/WebApiConfig.cs
+++ aspnet/1-hello-world/App_Start/WebApiConfig.cs
@@ -12,10 +12,6 @@
 // License for the specific language governing permissions and limitations under
 // the License.
 
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
