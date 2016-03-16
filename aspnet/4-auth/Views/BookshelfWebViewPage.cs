c997a9186beb59e5400f1889ec4dbd4a2545a568
remi Taylor <remily@google.com>
2016-03-16 10:22:08 -0700

    Update custom_web_view doc region to only include BookshelfWebViewPage<T> class

--- aspnet/4-auth/Views/BookshelfWebViewPage.cs
+++ aspnet/4-auth/Views/BookshelfWebViewPage.cs
@@ -1,14 +1,14 @@
 ﻿using System.Web.Mvc;
 using GoogleCloudSamples.Models;
 
-// [START custom_web_view]
 namespace GoogleCloudSamples.Views
 {
+    // [START custom_web_view]
     public abstract class BookshelfWebViewPage<TModel> : WebViewPage<TModel>
     {
         public User CurrentUser => new User(this.User);
     }
+    // [END custom_web_view]
 
     public abstract class BookshelfWebViewPage : WebViewPage { }
 }
-// [END custom_web_view]
