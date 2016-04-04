ab28714febad2242ffa8bd25042d16cfc40f035d
Jeffrey Rennie <rennie@google.com>
2016-04-04 09:47:02 -0700

    5-pubsub/worker now displays the work its doing.
    I also had to update all package dependencies to avoid version conflict
    issues.
    
    Uses SignalR to push updates to web page.

---
+++ aspnet/5-pubsub/worker/App_Start/BundleConfig.cs
@@ -0,0 +1,31 @@
+﻿// Copyright(c) 2016 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+using System.Web.Optimization;
+
+namespace GoogleCloudSamples
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+        }
+    }
+}
