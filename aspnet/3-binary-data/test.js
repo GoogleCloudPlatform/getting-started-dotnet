542c36ac5a4d2b05d966d907267dfda36b3b92b2
Jonathan Simon <jbsimon@google.com>
2016-05-03 13:00:26 -0700

    Update form autosubmit to false

--- aspnet/3-binary-data/test.js
+++ aspnet/3-binary-data/test.js
@@ -39,7 +39,7 @@ casper.thenClick('#add-book', function () {
         'Book.PublishedDate': '2000-01-01',
         'Book.Description': 'Automatically added by test.js',
         'image': fileName
-    }, true);
+    }, false);
     console.log('Filled form.');
     this.wait(1000);
     console.log('Submitting form.');
