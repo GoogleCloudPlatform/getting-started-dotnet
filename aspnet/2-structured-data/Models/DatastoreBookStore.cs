1b4510cd4cbe703e161c0a54b39842ecc0a7cfb3
Jeffrey Rennie <rennie@google.com>
2016-05-26 11:40:11 -0700

    Stay on target...

--- aspnet/2-structured-data/Models/DatastoreBookStore.cs
+++ aspnet/2-structured-data/Models/DatastoreBookStore.cs
@@ -12,70 +12,20 @@
 // License for the specific language governing permissions and limitations under
 // the License.
 
-using Google.Apis.Datastore.v1beta2;
-using Google.Apis.Datastore.v1beta2.Data;
+using Google.Datastore.V1Beta3;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+
+using static Google.Datastore.V1Beta3.CommitRequest.Types;
+using static Google.Datastore.V1Beta3.PropertyFilter.Types;
+using static Google.Datastore.V1Beta3.PropertyOrder.Types;
+using static Google.Datastore.V1Beta3.ReadOptions.Types;
 
 namespace GoogleCloudSamples.Models
 {
     public static class DatastoreBookStoreExtensionMethods
     {
-        /// <summary>
-        /// Make a datastore key given a book's id.
-        /// </summary>
-        /// <param name="id">A book's id.</param>
-        /// <returns>A datastore key.</returns>
-        public static Key ToKey(this long id)
-        {
-            return new Key()
-            {
-                Path = new KeyPathElement[]
-                {
-                    new KeyPathElement() { Kind = "Book", Id = (id == 0 ? (long?)null : id) }
-                }
-            };
-        }
-
-        /// <summary>
-        /// Make a book id given a datastore key.
-        /// </summary>
-        /// <param name="key">A datastore key</param>
-        /// <returns>A book id.</returns>
-        public static long ToId(this Key key)
-        {
-            return (long)key.Path.First().Id;
-        }
-
-        /// <summary>
-        /// Get the property from the dict and return null if it isn't there.
-        /// </summary>
-        /// <param name="properties"></param>
-        /// <param name="key"></param>
-        /// <returns></returns>
-        public static Property GetValue(this IDictionary<string, Property> properties, string key)
-        {
-            Property value;
-            bool found = properties.TryGetValue(key, out value);
-            return found ? value : null;
-        }
-
-        /// <summary>
-        /// Creates a new property iff value is not null.
-        /// </summary>
-        public static Property NewProperty(string value)
-        {
-            return null == value ? null : new Property() { StringValue = value };
-        }
-
-        public static Property NewProperty(DateTime? value)
-        {
-            return null == value ? null : new Property() { DateTimeValue = value };
-        }
-
-        // TODO: Use reflection so we don't have to modify the code every time we add or drop
-        // a property from Book.
         /// <summary>
         /// Create a datastore entity with the same values as book.
         /// </summary>
@@ -85,15 +35,13 @@ namespace GoogleCloudSamples.Models
         public static Entity ToEntity(this Book book)
         {
             var entity = new Entity();
-            entity.Properties =
-                new Dictionary<string, Property>();
             entity.Key = book.Id.ToKey();
-            entity.Properties["Title"] = NewProperty(book.Title);
-            entity.Properties["Author"] = NewProperty(book.Author);
-            entity.Properties["PublishedDate"] = NewProperty(book.PublishedDate);
-            entity.Properties["ImageUrl"] = NewProperty(book.ImageUrl);
-            entity.Properties["Description"] = NewProperty(book.Description);
-            entity.Properties["CreateById"] = NewProperty(book.CreatedById);
+            entity["Title"] = book.Title;
+            entity["Author"] = book.Author;
+            entity["PublishedDate"] = book.PublishedDate;
+            entity["ImageUrl"] = book.ImageUrl;
+            entity["Description"] = book.Description;
+            entity["CreateById"] = book.CreatedById;
             return entity;
         }
         // [END toentity]
@@ -105,16 +53,23 @@ namespace GoogleCloudSamples.Models
         /// <returns>A book.</returns>
         public static Book ToBook(this Entity entity)
         {
-            // TODO: Use reflection so we don't have to modify the code every time we add or drop
-            // a property from Book.
             Book book = new Book();
             book.Id = (long)entity.Key.Path.First().Id;
-            book.Title = entity.Properties.GetValue("Title")?.StringValue;
-            book.Author = entity.Properties.GetValue("Author")?.StringValue;
-            book.PublishedDate = entity.Properties.GetValue("PublishedDate")?.DateTimeValue;
-            book.ImageUrl = entity.Properties.GetValue("ImageUrl")?.StringValue;
-            book.Description = entity.Properties.GetValue("Description")?.StringValue;
-            book.CreatedById = entity.Properties.GetValue("CreatedById")?.StringValue;
+            // Having to call ?.StringValue is annoying.  In C++, the Value type would
+            // be automatically castable to these other types:
+            // class Value {
+            //   operator string()();
+            // Not sure if that's possible in C#.
+            // An alternative might be:
+            //   book.Title = entity.GetString("Title");
+            // Not sure I like it better.
+            book.Title = entity["Title"]?.StringValue;
+            book.Author = entity["Author"]?.StringValue;
+            // TimestampValue doesn't seem very useful.
+            book.PublishedDate = entity["PublishedDate"]?.TimestampValue.ToDateTime();
+            book.ImageUrl = entity["ImageUrl"]?.StringValue;
+            book.Description = entity["Description"]?.StringValue;
+            book.CreatedById = entity["CreatedById"]?.StringValue;
             return book;
         }
     }
@@ -122,7 +77,7 @@ namespace GoogleCloudSamples.Models
     public class DatastoreBookStore : IBookStore
     {
         private readonly string _projectId;
-        private readonly DatastoreService _datastore;
+        private readonly DatastoreClient _datastore;
 
         /// <summary>
         /// Create a new datastore-backed bookstore.
@@ -132,51 +87,23 @@ namespace GoogleCloudSamples.Models
         {
             _projectId = projectId;
             // Use Application Default Credentials.
-            var credentials = Google.Apis.Auth.OAuth2.GoogleCredential
-                .GetApplicationDefaultAsync().Result;
-            if (credentials.IsCreateScopedRequired)
-            {
-                credentials = credentials.CreateScoped(new[] {
-                    DatastoreService.Scope.Datastore,
-                    DatastoreService.Scope.UserinfoEmail,
-                });
-            }
-            // Create our connection to datastore.
-            _datastore = new DatastoreService(new Google.Apis.Services
-                .BaseClientService.Initializer()
-            {
-                HttpClientInitializer = credentials,
-                ApplicationName = "Bookshelf.NET-Step2"
-            });
+            _datastore = DatastoreClient.Create();
         }
-
-        /// <summary>
-        /// A convenience function which commits a mutation to datastore.
-        /// Use this function to avoid a lot of boilerplate.
-        /// </summary>
-        /// <param name="mutation">The change to make to datastore.</param>
-        /// <returns>The result of commiting the change.</returns>
-        // [START commitmutation]
-        private CommitResponse CommitMutation(Mutation mutation)
-        {
-            var commitRequest = new CommitRequest()
-            {
-                Mode = "NON_TRANSACTIONAL",
-                Mutation = mutation
-            };
-            return _datastore.Datasets.Commit(commitRequest, _projectId)
-                .Execute();
-        }
-        // [END commitmutation]
 
         // [START create]
         public void Create(Book book)
         {
-            var result = CommitMutation(new Mutation()
-            {
-                InsertAutoId = new Entity[] { book.ToEntity() }
-            });
-            book.Id = result.MutationResult.InsertAutoIdKeys.First().Path.First().Id.Value;
+            // Why do I have to pass the _projectId again here if it's already in the key?
+            // Actually, I want the projectId baked into the datastore client so I never have
+            // to specify it again.
+            // Calling .ToInsert() is very weird.
+            // Having two very different ways to insert, update, etc. depending on whether or not
+            // I'm in a transaction is annoying.  I want one interface to do it.
+            // How about a NullTransaction where all operations are immediately committed, and the
+            // final .Commit() is a no-op?
+            CommitResponse response = _datastore.Commit(_projectId, Mode.NonTransactional, new[] { book.ToEntity().ToInsert() });
+            Key key = response.MutationResults[0].Key;
+            book.Id = key.Path.First().Id;
         }
         // [END create]
 
