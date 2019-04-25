1367b03a9fe605a8a1dea61c0cd2c800f7239354
Jeffrey Rennie <rennie@google.com>
2019-04-25 11:31:58 -0700

    UseGoogleDiagnostics
    Change-Id: Ia41361e1906b375beed311e92b2d7155ddcd2e83

--- Bookshelf/Program.cs
+++ Bookshelf/Program.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Google.Cloud.Diagnostics.AspNetCore;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Bookshelf
 {
@@ -19,6 +20,7 @@ namespace Bookshelf
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseGoogleDiagnostics(Startup.GetProjectId(), "Bookshelf", "0.01")
                 .UseStartup<Startup>();
     }
 }
