78646df10a037a0861a20513b972682ad5d0195c
Jeffrey Rennie <rennie@google.com>
2019-04-29 14:15:28 -0700

    Lint

--- Bookshelf/Startup.cs
+++ Bookshelf/Startup.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿// Copyright (c) 2019 Google LLC.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -29,7 +43,7 @@ namespace Bookshelf
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<Services.ImageUploader>(provider  => 
+            services.AddSingleton<Services.ImageUploader>(provider =>
                 new Services.ImageUploader(Configuration["Bucket"]));
             // Choose a BookStoreBackend.
             BookStoreBackend backend = Enum.Parse<BookStoreBackend>(
@@ -43,7 +57,7 @@ namespace Bookshelf
                     services.AddScoped<IBookStore, DbBookStore>();
                     break;
                 case BookStoreBackend.Firestore:
-                    services.AddSingleton<IBookStore>(provider => 
+                    services.AddSingleton<IBookStore>(provider =>
                         new FirestoreBookStore(GetProjectId()));
                     break;
                 case BookStoreBackend.SqlServer:
@@ -57,11 +71,11 @@ namespace Bookshelf
                         .AddDbContext<BookStoreDbContext>(options =>
                             options.UseNpgsql(Configuration.GetConnectionString("Npgsql")));
                     services.AddScoped<IBookStore, DbBookStore>();
-                    break;                    
+                    break;
                 default:
                     throw new NotImplementedException(backend.ToString());
             }
-            
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
