12e189be1c47db0cd156da43f8116c8a425b9241
Jeffrey Rennie <rennie@google.com>
2019-07-24 13:15:54 -0700

    A simple sessions demo.
    Change-Id: I73def5216f56e2ad5a22df5fb532c1435f189997

---
+++ Sessions/Startup.cs
@@ -0,0 +1,50 @@
+﻿using Google.Cloud.AspNetCore.Firestore.DistributedCache;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Sessions
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddFirestoreDistributedCache("surferjeff-firestore")
+                .AddFirestoreDistributedCacheGarbageCollector();
+            services.AddSession();
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseSession();
+            app.UseCookiePolicy();
+
+            app.Run(async (context) =>
+            {
+                var views = context.Session.GetInt32("views").GetValueOrDefault();
+                views += 1;
+                context.Session.SetInt32("views" , views);
+                string greeting = "Hello World";
+                await context.Response.WriteAsync($"{views} views for {greeting}.");
+            });
+        }
+    }
+}
