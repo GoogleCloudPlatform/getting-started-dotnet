ee7a57c839012033fa6bdf469d83c7e095764600
Jeffrey Rennie <rennie@google.com>
2019-08-23 08:54:52 -0700

    Downgrade bookshelf to dotnet 2.1
    Change-Id: I30742cd09d4ebe0cb930dfd8146314f26b87f53a

--- Bookshelf/Startup.cs
+++ Bookshelf/Startup.cs
@@ -79,7 +79,7 @@ namespace Bookshelf
                     throw new NotImplementedException(backend.ToString());
             }
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
